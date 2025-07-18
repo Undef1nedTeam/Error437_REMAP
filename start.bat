8bf95b6fcd855c36ec16341e8b053c5024b5b47c
ImWuMie <110892807+ImWuMie@users.noreply.github.com>
2025-07-18 20:21:41 +0800

    init

---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+java -Djava.library.path=libs/natives -cp libs/codecjorbis-20101023.jar;libs/codecwav-20101023.jar;libs/jinput.jar;libs/libraryjavasound-20101123.jar;libs/librarylwjglopenal-20100824.jar;libs/lwjgl.jar;libs/lwjgl_util.jar;libs/soundsystem-20120107.jar;Error437O.jar Start
+pause
